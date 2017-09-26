beb856ecad6bbdd17c56479791b05414ad56018f
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-09-26 14:04:29 -0600

    Bower install (Librerías esenciales), Enlazadas librerías locales

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -5,12 +5,33 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link href="{{ mix('/css/all.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="{{ asset('bower/bootstrap/dist/css/bootstrap.min.css') }}">
+{{--<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">--}}
+<!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('bower/font-awesome/css/font-awesome.min.css') }}">
+{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">--}}
+<!-- Ionicons -->
+    <link rel="stylesheet" href="{{ asset('bower/Ionicons/css/ionicons.min.css') }}">
+{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">--}}
+<!-- jvectormap -->
+    <link rel="stylesheet" href="{{ asset('bower/jvectormap/jquery-jvectormap.css') }}">
+{{--<link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">--}}
+<!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('bower/AdminLTE/dist/css/AdminLTE.min.css') }}">
+{{--<link rel="stylesheet" href="dist/css/AdminLTE.min.css">--}}
+<!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="{{ asset('bower/AdminLTE/dist/css/skins/_all-skins.min.css') }}">
+{{--<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">--}}
+<!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('bower/iCheck/skins/square/_all.css') }}">
+
+
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
--- resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
@@ -2,9 +2,40 @@
 
 <!-- JQuery and bootstrap are required by Laravel 5.3 in resources/assets/js/bootstrap.js-->
 <!-- Laravel App -->
-<script src="{{ url (mix('/js/app.js')) }}" type="text/javascript"></script>
-
+{{--<script src="{{ url (mix('/js/app.js')) }}" type="text/javascript"></script>--}}
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
       Both of these plugins are recommended to enhance the
       user experience. Slimscroll is required when using the
       fixed layout. -->
+
+
+<!-- jQuery 2.2.3 -->
+<script src="{{ asset('bower/jquery/dist/jquery.min.js') }}" type="text/javascript"></script>
+{{--<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>--}}
+
+<!-- Bootstrap 3.3.6 -->
+<script src="{{ asset('bower/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
+{{--<script src="bootstrap/js/bootstrap.min.js"></script>--}}
+
+<!-- AdminLTE App -->
+<script src="{{ asset('bower/AdminLTE/dist/js/app.min.js') }}" type="text/javascript"></script>
+{{--<script src="dist/js/app.min.js"></script>--}}
+
+<!-- icheck -->
+<script src="{{ asset('bower/iCheck/icheck.min.js') }}" type="text/javascript"></script>
+
+{{--<!-- FastClick -->--}}
+{{--<script src="plugins/fastclick/fastclick.js"></script>--}}
+{{--<!-- Sparkline -->--}}
+{{--<script src="plugins/sparkline/jquery.sparkline.min.js"></script>--}}
+{{--<!-- jvectormap -->--}}
+{{--<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>--}}
+{{--<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>--}}
+{{--<!-- SlimScroll 1.3.0 -->--}}
+{{--<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>--}}
+{{--<!-- ChartJS 1.0.1 -->--}}
+{{--<script src="plugins/chartjs/Chart.min.js"></script>--}}
+{{--<!-- AdminLTE dashboard demo (This is only for demo purposes) -->--}}
+{{--<script src="dist/js/pages/dashboard2.js"></script>--}}
+{{--<!-- AdminLTE for demo purposes -->--}}
+{{--<script src="dist/js/demo.js"></script>--}}
