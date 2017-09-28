d4001d1b7c5ffdcd55a4348f2e202d59774e3d46
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-09-28 08:19:11 -0600

    Enlazadas localmente librerías en layout datatables css y js

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -1,4 +1,7 @@
 <!-- DataTable Bootstrap -->
-<link href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.bootstrap.min.css">
+{{--        <link rel="stylesheet" href="{{asset('plugins/DataTables/datatables.min.css')}}">--}}
+<link rel="stylesheet" href="{{asset('bower/datatables/media/css/dataTables.bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('plugins/DataTables/Responsive-2.1.1/css/responsive.bootstrap.min.css')}}">
+{{--<link href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet">--}}
+{{--<link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">--}}
+{{--<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.bootstrap.min.css">--}}
