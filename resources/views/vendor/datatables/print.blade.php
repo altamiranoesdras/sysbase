97c9930b87fb164d528c61cf0c4b70f7d518df71
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-11-07 15:49:22 -0600

    Librerías locales para imprimir o exportar a pdf las datatables

--- resources/views/vendor/datatables/print.blade.php
+++ resources/views/vendor/datatables/print.blade.php
@@ -6,8 +6,8 @@
         <meta name=description content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap CSS -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ asset('bower/bootstrap/dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('bower/font-awesome/css/font-awesome.min.css') }}">
         <style>
             body {margin: 20px}
         </style>
