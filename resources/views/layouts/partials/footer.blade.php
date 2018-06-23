80e85d2d39fb95e165fc64b5fb29c075b7b4f531
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-06-23 14:59:07 -0600

    Agregado favicon editado el footer y mejorado editar perfil

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -5,5 +5,5 @@
         Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2018 <a href="https://solucionesaltamirano.com/">Soluciones Altamirano</a>.</strong> All rights reserved.
 </footer>
--- resources/views/users/edit_profile.blade.php
+++ resources/views/users/edit_profile.blade.php
@@ -7,24 +7,44 @@
 @endsection
 
 @section('content')
-    <section class="content-header">
-        <h1>
-            Editar perfil
-        </h1>
-   </section>
-   <div class="content">
-       @include('flash::message')
-       @include('adminlte-templates::common.errors')
-       <div class="box box-primary">
-           <div class="box-body">
-               <div class="row">
-                   {!! Form::model($user, ['route' => ['user.update.profile', $user->id], 'method' => 'patch','enctype' => "multipart/form-data"]) !!}
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Editar perfil</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-                        @include('users.fields_profile')
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            @include('flash::message')
+            @include('adminlte-templates::common.errors')
 
-                   {!! Form::close() !!}
-               </div>
-           </div>
-       </div>
-   </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                {!! Form::model($user, ['route' => ['user.update.profile', $user->id], 'method' => 'patch','enctype' => "multipart/form-data"]) !!}
+
+                                @include('users.fields_profile')
+
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col-md-6 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
 @endsection
