7b32da11d940cd5f57c68737f1dfb3c4fd3846c6
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-06-26 13:08:06 -0600

    Fix index titulo y boton nuevo

--- resources/views/configurations/index.blade.php
+++ resources/views/configurations/index.blade.php
@@ -8,12 +8,12 @@
     <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
+            <div class="row">
+                <div class="col">
                     <h1 class="m-0 text-dark">Configurations</h1>
                 </div><!-- /.col -->
-                <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
+                <div class="col">
+                        <ol class="breadcrumb float-right">
                             <li class="breadcrumb-item">
                                 <a class="btn btn-outline-success"
                                     href="{!! route('configurations.create') !!}">
