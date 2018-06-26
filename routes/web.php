40ef68396683b1b59247248bf2739223ec0b9d18
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-06-25 20:25:19 -0600

    Fix rout home

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('pruebas', function () {
 });
 
 Route::get('/', 'HomeController@index');
-Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
     //    Route::get('/link1', function ()    {
