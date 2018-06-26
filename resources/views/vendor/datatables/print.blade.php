fa1a5787e629310235c05ba447ae72b6d0e12c30
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-06-26 10:35:58 -0600

    Fix print table yajra datatables

--- resources/views/vendor/datatables/print.blade.php
+++ resources/views/vendor/datatables/print.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Print Table</title>
+        <title> @yield('htmlheader_title', config('app.name')) </title>
         <meta charset="UTF-8">
         <meta name=description content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap CSS -->
-        <link href="{{ mix('/css/all.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
         <style>
             body {margin: 20px}
         </style>
