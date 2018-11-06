04e0ad8fb1c751806fa058ba9cbe5de3e5e03686
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-11-06 16:44:05 -0600

    Fix script langs adminilte

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -21,6 +21,21 @@
     <!--App css-->
     @yield('css')
     @stack('css')
-    {{ $css or ''}}
 
+    
+    <!-- Script para comvertir en json los archivos de lenguaje del paquete adminlte -->
+    <script>
+        //See https://laracasts.com/discuss/channels/vue/use-trans-in-vuejs
+        window.trans = @php
+            // copy all translations from /resources/lang/CURRENT_LOCALE/* to global JS variable
+            $lang_files = File::files(resource_path() . '/lang/' . App::getLocale());
+            $trans = [];
+            foreach ($lang_files as $f) {
+                $filename = pathinfo($f)['filename'];
+                $trans[$filename] = trans($filename);
+            }
+            $trans['adminlte_lang_message'] = trans('adminlte_lang::message');
+            echo json_encode($trans);
+        @endphp
+    </script>
 </head>
