a43f232885445723b9905192c133ab11533782a1
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-11-07 14:45:51 -0600

    Compilación y registro de serviceWorker

--- webpack.mix.js
+++ webpack.mix.js
@@ -38,6 +38,38 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .copy('node_modules/moment/min/moment.min.js','public/js/moment')
     .copy('node_modules/moment/locale/es.js','public/js/moment')
 
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+mix.webpackConfig({
+    plugins: [
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'pwa',
+            filename: 'service-worker.js',
+            staticFileGlobs: ['public/**/*.{css,eot,svg,ttf,woff,woff2,js,html}'],
+            minify: true,
+            stripPrefix: 'public/',
+            handleFetch: true,
+            dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
+                //and have full support for offline first (example below)
+                '/': ['resources/views/welcome.blade.php'],
+                // '/posts': ['resources/views/posts.blade.php']
+            },
+            staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+            navigateFallback: '/',
+            runtimeCaching: [
+                {
+                    urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+                    handler: 'cacheFirst'
+                },
+                {
+                    urlPattern: /^https:\/\/www\.thecocktaildb\.com\/images\/media\/drink\/(\w+)\.jpg/,
+                    handler: 'cacheFirst'
+                }
+            ],
+            // importScripts: ['./js/push_message.js']
+        })
+    ]
+});
+
 // mix.browserSync({
 //     proxy: 'http://negocios.local',
 //     open: false
