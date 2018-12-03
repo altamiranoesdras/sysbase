a5ae3c942578bdf5c9267c627c669a76585e47ce
Esdras Altamirano <altamiranoesdras@gmail.com>
2018-12-03 15:22:18 -0600

    Fix print datatable print.blade

--- resources/views/vendor/datatables/print.blade.php
+++ resources/views/vendor/datatables/print.blade.php
@@ -6,7 +6,7 @@
         <meta name=description content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap CSS -->
-        <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />.css">
+        <link href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     </head>
     <body onload="window.print()">
 
