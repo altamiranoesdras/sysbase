a90726ecb7c590d8d828a374bf9e29bd0b5a2434
Esdras Altamirano <altamiranoesdras@gmail.com>
2019-02-12 09:08:39 -0600

    Editado seeder de configuraciones

--- database/seeds/ConfigurationsTableSeeder.php
+++ database/seeds/ConfigurationsTableSeeder.php
@@ -15,14 +15,46 @@ class ConfigurationsTableSeeder extends Seeder
     {
 
 
-        factory(Configuration::class,1)->create(['key' => 'name','value' => 'ASISTENCIA']);
-        factory(Configuration::class,1)->create(['key' => 'tiempo_oculta_alerta','value' => '3000']);
-        factory(Configuration::class,1)->create(['key' => 'divisa','value' => 'Q']);
-        factory(Configuration::class,1)->create(['key' => 'cantidad_decimales','value' => '2']);
-        factory(Configuration::class,1)->create(['key' => 'separador_miles','value' => ',']);
-        factory(Configuration::class,1)->create(['key' => 'separador_decimal','value' => '.']);
-        factory(Configuration::class,1)->create(['key' => 'cantidad_decimales_precio','value' => '2']);
-        factory(Configuration::class,1)->create(['key' => 'mail_pruebas','value' => 'altamiranoesdras@gmail.com']);
+        factory(Configuration::class,1)->create([
+            'key' => 'name',
+            'value' => 'ASISTENCIA',
+            'descripcion' => 'Nombre de la aplicacion'
+        ]);
+        factory(Configuration::class,1)->create([
+            'key' => 'tiempo_oculta_alerta',
+            'value' => '3000',
+            'descripcion' => 'Tiempo en milisegundos para ocultar las alertas de exito o error'
+        ]);
+        factory(Configuration::class,1)->create([
+            'key' => 'divisa',
+            'value' => 'Q',
+            'descripcion' => 'Símbolo de la moneda que se utiliza'
+        ]);
+        factory(Configuration::class,1)->create([
+            'key' => 'cantidad_decimales',
+            'value' => '2',
+            'descripcion' => 'Cantidad de decimales para cantidades que no sean de precio '
+        ]);
+        factory(Configuration::class,1)->create([
+            'key' => 'separador_miles',
+            'value' => ',',
+            'descripcion' => 'Símbolo para separa los miles o millares en las cantidades'
+        ]);
+        factory(Configuration::class,1)->create([
+            'key' => 'separador_decimal',
+            'value' => '.',
+            'descripcion' => 'Símbolo para separar los decimales en las cantidades'
+        ]);
+        factory(Configuration::class,1)->create([
+            'key' => 'cantidad_decimales_precio',
+            'value' => '2',
+            'descripcion' => 'Cantidad de decimales para cantidades de precio'
+        ]);
+        factory(Configuration::class,1)->create([
+            'key' => 'mail_pruebas',
+            'value' => 'altamiranoesdras@gmail.com',
+            'descripcion' => 'Email al que se envían los correos cuando el entorno de la aplicación esta en modo debug o local'
+        ]);
 
     }
 }
