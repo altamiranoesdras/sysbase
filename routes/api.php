6379220990e2765a6e3d723e2debaf51509fcf09
Esdras Altamirano <altamiranoesdras@gmail.com>
2019-02-12 08:11:44 -0600

    Cambios en rutas api para que tome el middleware auth:api

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function () {
+
+    Route::resource('permissions', 'PermissionAPIController');
+
+    Route::resource('roles', 'RoleAPIController');
 });
-
-Route::group(['prefix' => 'v1','middleware' => 'auth:api'], function () {
-    //    Route::resource('task', 'TasksController');
-
-    //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
-    #adminlte_api_routes
-});
-
-
-Route::resource('permissions', 'PermissionAPIController');
-
-Route::resource('roles', 'RoleAPIController');
